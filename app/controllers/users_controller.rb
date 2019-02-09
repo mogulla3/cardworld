b833b97b6d93366228bfed999007663273e52c53
mogulla3 <hs.iarn3@gmail.com>
2019-02-09 12:26:02 +0900

    Fix controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,9 @@
 class UsersController < ApplicationController
   def index
-    # render json: User.all
     @users = User.all.decorate
-    @users2 = UserDecorator.decorate_collection(User.all)
   end
 
   def show
     @user = User.includes(:decks).find_by(id: params[:user_id]).decorate
-    # render json: User.find_by(id: params[:user_id])
   end
 end
